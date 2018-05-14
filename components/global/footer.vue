f0930632bd0d22b825d106d5f414bac499ce3835
Sean Washington <hello@seanwash.com>
2018-05-13 21:54:55 -0400

    Update footer

--- components/global/footer.vue
+++ components/global/footer.vue
@@ -1,29 +1,23 @@
 <template>
-  <footer class="mw9 center mt4 f6">
+  <footer class="mw9 center mv4 f6">
     <div class="bt pv4">
-      <a class="dib link black dim b tracked"
-         href="/">Sean Washington</a>
+      <div class="fl mr4">
+          <a href="/" class="dib link black dim b tracked">Paul Straw</a>
 
-      <ul class="mt2 ma0 pa0">
-        <li class="dib">
-          <a class="link black dim"
-             href="http://github.com/seanwash">GitHub</a>
-        </li>
-        <li class="dib">
-          <a class="link black dim ml2"
-             href="https://spec.fm/podcasts/does-not-compute">Spec.fm</a>
-        </li>
-        <li class="dib ml2">
-          <a class="link black dim"
-             href="http://twitter.com/seanwashbot">Twitter</a>
-        </li>
-      </ul>
+          <ul class="mt2 ma0 pa0">
+            <li class="dib"><a href="http://github.com/paulstraw" class="link black dim">GitHub</a></li>
+            <li class="dib ml2"><a href="http://twitter.com/paulstraw" class="link black dim">Twitter</a></li>
+          </ul>
+        </div>
+
+      <div class="fl">
+        <a href="/" class="dib link black dim b tracked">Sean Washington</a>
+
+        <ul class="mt2 ma0 pa0">
+          <li class="dib"><a href="http://github.com/seanwash" class="link black dim">GitHub</a></li>
+          <li class="dib ml2"><a href="http://twitter.com/seanwashbot" class="link black dim">Twitter</a></li>
+        </ul>
+      </div>
     </div>
   </footer>
 </template>
-
-<script>
-export default {
-  name: 'Footer'
-}
-</script>
