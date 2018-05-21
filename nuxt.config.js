eb1f9d3371efd124823c259d2de18a768fefa548
Sean Washington <hello@seanwash.com>
2018-05-20 22:13:44 -0400

    Add some meta info

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 
+let description =
+  'A weekly podcast about the lives and workflows of modern web developers, hosted by Sean Washington & Paul Straw.'
+
 module.exports = {
   // prettier-ignore
   head: {
@@ -7,9 +10,13 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: description },
+      { hid: 'description', name: 'description', content: description },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'alernate', type:'application/rss+xml', title: "Does Not Compute RSS", href: "https://rss.simplecast.com/podcasts/1386/rss" }
+    ]
   },
   css: ['tachyons/css/tachyons.css'],
   loading: { color: '#3B8070' },
