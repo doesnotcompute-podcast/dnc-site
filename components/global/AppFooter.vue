3263f7747561307aea2caffc732618e6c0c9f32f
Sean Washington <hello@seanwash.com>
2018-07-21 10:41:54 -0700

    Update app footer with up to date contributors

--- components/global/AppFooter.vue
+++ components/global/AppFooter.vue
@@ -1,23 +1,58 @@
 <template>
   <footer class="mw9 center mv4 f6">
     <div class="bt pv4">
-      <div class="fl mr4">
-          <a href="/" class="dib link black dim b tracked">Paul Straw</a>
-
-          <ul class="mt2 ma0 pa0">
-            <li class="dib"><a href="http://github.com/paulstraw" class="link black dim">GitHub</a></li>
-            <li class="dib ml2"><a href="http://twitter.com/paulstraw" class="link black dim">Twitter</a></li>
-          </ul>
-        </div>
-
-      <div class="fl">
-        <a href="/" class="dib link black dim b tracked">Sean Washington</a>
+      <div
+        v-for="contrib in contributors"
+        :key="contrib.name"
+        class="fl mr4"
+      >
+        <a
+          href="/"
+          class="dib link black dim b tracked"
+          v-text="contrib.name"
+        />
 
         <ul class="mt2 ma0 pa0">
-          <li class="dib"><a href="http://github.com/seanwash" class="link black dim">GitHub</a></li>
-          <li class="dib ml2"><a href="http://twitter.com/seanwashbot" class="link black dim">Twitter</a></li>
+          <li class="dib">
+            <a
+              :href="contrib.github"
+              class="link black dim"
+            >GitHub</a>
+          </li>
+          <li class="dib ml2">
+            <a
+              :href="contrib.twitter"
+              class="link black dim"
+            >Twitter</a>
+          </li>
         </ul>
       </div>
     </div>
   </footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      contributors: [
+        {
+          name: 'Sean Washington',
+          github: 'https://github.com/seanwash',
+          twitter: 'https://twitter.com/seanwashbot'
+        },
+        {
+          name: 'Rockwell Schrock',
+          github: 'https://github.com/schrockwell',
+          twitter: 'https://twitter.com/schrockwell'
+        },
+        {
+          name: 'Mikhail Delport',
+          github: 'https://github.com/mikhailbot',
+          twitter: 'https://twitter.com/_mikhailbot'
+        }
+      ]
+    }
+  }
+}
+</script>
